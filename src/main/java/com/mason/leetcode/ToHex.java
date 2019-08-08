96cc7c9529f79462959b8d65740c4ca2657aa7d8
huangsiqian <huangsiqian>
2019-08-08 17:53:52 +0800

    16进制转换

---
+++ src/main/java/com/mason/leetcode/ToHex.java
@@ -0,0 +1,26 @@
+package com.mason.leetcode;
+
+/**
+ * Created by huang_sq on 2019/8/8.
+ */
+public class ToHex {
+
+
+    public static String toHex(int num) {
+        if (num == 0)
+            return "0";
+        char[] chars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+        StringBuilder sb = new StringBuilder();
+        while (sb.length() < 8 && num != 0) {
+            sb.append(chars[num & 0xf]);
+            num >>= 4;
+        }
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(toHex(28));
+
+    }
+
+}
