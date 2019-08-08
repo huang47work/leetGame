1487a58376833a2db6fc10dd1bb9189e051f84e0
huangsiqian <huangsiqian>
2019-08-08 13:23:45 +0800

    init

---
+++ src/main/java/com/mason/leetcode/BinaryWatch.java
@@ -0,0 +1,14 @@
+package com.mason.leetcode;
+
+/**
+ * Created by huang_sq on 2019/8/8.
+ */
+public class BinaryWatch {
+
+    public static void main(String[] args) {
+        System.out.println("set");
+    }
+
+
+
+}
