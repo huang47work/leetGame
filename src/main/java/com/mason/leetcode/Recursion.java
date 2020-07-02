d29d80c4fa79360229da129f699e4ff32512819a
huang47work <huang47work@gmail.com>
2020-07-02 16:18:44 +0800

    Update Recursion.java

--- src/main/java/com/mason/leetcode/Recursion.java
+++ src/main/java/com/mason/leetcode/Recursion.java
@@ -8,12 +8,31 @@ public class Recursion {
 
 
     public static void main(String[] args) {
-//        int[] a = {1, 3, 4, 8, 9, 1, 2, 4, 0, 7, 6};
-//        bubbleSort(a);
 
-//        insertSort(a);
+        int[] a = {1, 3, 4, 8, 9, 1, 2, 4, 0, 7, 6};
+        int[] sortedArray = {0, 1, 1, 2, 3, 4, 4, 6, 7, 8, 9};
+        //                        bubbleSort(a);
 
-        findRoot("3", 7);
+        //        insertSort(a);
+
+        //        findRoot("3", 7);
+        int value = 4;
+        /*int index = binarySearchFirstMin(sortedArray, value);
+        if (index > 0) {
+            System.out.println("第一个大于等于" + value + "的元素是下表为" + index + "的元素" + sortedArray[index]);
+        } else {
+            System.out.println("找不到你要的元素哦");
+        }*/
+
+
+        int index = binarySearchLastMin(sortedArray, value);
+        if (index > 0) {
+            System.out.println("最后一个小于等于" + value + "的元素是下表为" + index + "的元素" + sortedArray[index]);
+        } else {
+            System.out.println("找不到你要的元素哦");
+        }
+
+
     }
 
     private static void bubbleSort(int[] a) {
@@ -87,6 +106,77 @@ public class Recursion {
             mid = high.add(low).divide(new BigDecimal("2"));
         }
         System.out.println(a + "的平方根=" + mid.setScale(n, RoundingMode.UP));
+    }
+
+    /**
+     * 二分查找第一个大于给定数value的元素
+     * @param a 数组
+     * @param value 给定的数
+     *             此处给的数组有序 , 但是有重复的元素
+     */
+    private static int binarySearchFirstMin(int[] a, int value) {
+        int length = a.length;
+        int low = 0;
+        int high = length - 1;
+
+        //边界判断
+        if (a[low] >= value) {
+            return low;
+        } else if (a[high] < value) {
+            return -1;
+        }
+
+        int mid;
+        while (low <= high) {
+            mid = low + ((high - low) >> 1);
+            if (a[mid] >= value) {
+                if (a[mid] >= value && a[mid - 1] < value) {
+                    return mid;
+                }
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+
+    }
+
+
+    /**
+     * 二分查找最后一个元素小于给定数value的元素
+     * @param a 数组
+     * @param value 给定的数
+     *             此处给的数组有序 , 但是有重复的元素
+     */
+    private static int binarySearchLastMin(int[] a, int value) {
+        int length = a.length;
+        int low = 0;
+        int high = length - 1;
+
+        //边界判断
+        //最大的都比它小
+        if (a[high] < value) {
+            return high;
+        //最小都比它大
+        } else if (a[low] >= value) {
+            return -1;
+        }
+
+        int mid;
+        while (low <= high) {
+            mid = low + ((high - low) >> 1);
+            if (a[mid] <= value) {
+                if (a[mid] <= value && a[mid + 1] > value) {
+                    return mid;
+                }
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+
     }
 
 
